994011f62f0d3e29330cc543f76fa63c5c84ddf6
Christopher Collins <chris.collins@glovoapp.com>
2020-03-21 11:40:49 +0100

    completed train/predict scripts

--- src/predict.py
+++ src/predict.py
@@ -1,10 +1,37 @@
+import pandas as pd
+
 from src.models.MatchResultXGBoostModel import MatchResultXGBoost
+from src.utils.api import get_upcoming_games
+from src.update_tables import update_tables
+from src.utils.db import run_query, connect_to_db
+
+
+def predict():
+    # Get the current season
+    season = run_query(query='select max(season) from main_fixtures').iloc[0, 0]
+    # Update tables
+    update_tables()
+    # Load the in-production model
+    model = MatchResultXGBoost(load_trained_model=True, problem_name='match-predict-base')
+    # Get upcoming games (that we haven't predicted)
+    df = get_upcoming_games()
+    # Make predictions
+    predictions = pd.DataFrame(columns=['H', 'D', 'A'])
+    for row in df.iterrows():
+        input_dict = {
+            "date": row[1]['kickoff_time'],
+            "home_id": row[1]['home_id'],
+            "away_id": row[1]['away_id'],
+            "season": "19/20",
+            "home_odds": row[1]['home_odds'],
+            "draw_odds": row[1]['draw_odds'],
+            "away_odds": row[1]['away_odds']
+        }
+        predictions_df = pd.DataFrame(model.predict(**input_dict), index=[len(predictions)])
+        predictions = predictions.append(predictions_df)
+    with connect_to_db() as conn:
+        predictions.to_sql('latest_predictions', conn)
+
 
 if __name__ == '__main__':
-    model = MatchResultXGBoost(
-        load_trained_model=True,
-        problem_name='match-predict-base'
-    )
-    # Get upcoming games (that we havent predicted)
-
-    # Make predictions
+    predict()
--- src/train.py
+++ src/train.py
@@ -1,8 +1,16 @@
 from src.models.MatchResultXGBoostModel import MatchResultXGBoost
+from src.update_tables import update_tables
 
-if __name__ == '__main__':
-    new_model = MatchResultXGBoost(
+
+def train():
+    # Update tables
+    update_tables()
+    # Train new model
+    MatchResultXGBoost(
         upload_historic_predictions=True,
         problem_name='match-predict-base',
-        compare_models=True
-    )
+        compare_models=True)
+
+
+if __name__ == '__main__':
+    train()
