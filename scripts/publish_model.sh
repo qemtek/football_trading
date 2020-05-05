7d5fa6a01d2bf8321878410dd7938a3083d9a80b
Christopher Collins <chris.collins@glovoapp.com>
2020-05-05 17:07:59 +0100

    create build model package script

--- scripts/publish_model.sh
+++ scripts/publish_model.sh
@@ -1,27 +1,5 @@
 #!/bin/bash
 
-export IN_PRODUCTION=false
-export PROJECTSPATH=/home/circleci/project/packages/football_trading
-export DB_DIR=/home/circleci/project/db.sqlite
-export BFEX_USER=$CCI_BFEX_USER
-export BFEX_PASSWORD=$CCI_BFEX_PASS
-export BFEX_APP_KEY=$CCI_BFEX_APP_KEY
-export BFEX_CERTS_PATH=$CCI_BFEX_CERTS_PATH
-export PRODUCTION_MODEL_NAME=match-predict-base
-export RECREATE_DB=false
-export LOCAL=true
-
-cd ..
-virtualenv venv
-. venv/bin/activate
-ls
-pip install --upgrade pip
-pip install -r ./packages/football_trading/requirements.txt
-cd packages/football_trading
-tox
-cd scripts
-
-
 # Building packages and uploading them to a Gemfury repository
 GEMFURY_URL=$PIP_EXTRA_INDEX_URL
 
