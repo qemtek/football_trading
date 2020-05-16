d29ae272f8211ec1e42725f4a448ee600a511c79
Christopher Collins <chris.collins@glovoapp.com>
2020-05-16 18:48:27 +0100

    fix wrongly named env var

--- packages/ft_api/ft_api/__init__.py
+++ packages/ft_api/ft_api/__init__.py
@@ -1,4 +1,4 @@
-from ft_api.configuration import PACKAGE_ROOT
+from ft_api.settings import PROJECTSPATH
 
-with open(PACKAGE_ROOT / 'VERSION') as version_file:
+with open(f"{PROJECTSPATH}/VERSION") as version_file:
     __version__ = version_file.read().strip()
